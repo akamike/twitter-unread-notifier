a6e8f6459b259d298cd5825f00627c401e4f485f
Mike Robinson <mikekuul@gmail.com>
2010-09-21 21:44:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Twitter Unread Notifier
+
+A userscript for Fluid.app and twitter.com
+
+# # About
+
+This userscript can be used with your twitter.com instance of Fluid.app (SSB) to update the dock badge with the count of unread tweets. It also displays a growl notification with the unread count every 10 minutes, just in case you don't watch your dock.
+
+# # Install
+
+1. Download 'Twitter Unread Notifier.user.js'.
+2. Place the file into your Twitter SSB's userscript folder located at `~/Library/Application Support/Fluid/SSB/<your-ssb-name>/Userscripts`. You can also access this from your Twitter SSB by opening `Userscripts > Open Userscripts Folder`.
+3. In your Twitter SSB, `Userscripts > Reload All Userscripts`, and then `Userscripts > Twitter Unread Notifier`.
+
+And that's it! You may need to reload the page or restart your SSB to ensure it has been installed correctly.
---
+++ Twitter Unread Notifier.user.js
@@ -0,0 +1,54 @@
+// ==UserScript==
+// @name        Twitter Unread Notifier
+// @namespace   http://fluidapp.com
+// @description Display unread count for Twitter in dock with a Growl notification every few minutes
+// @include     http://twitter.com/*
+// @include     https://twitter.com/*
+// @include     http://*.twitter.com/*
+// @include     https://*.twitter.com/*
+// @include     http://twitter.com*
+// @include     http://twitter.com/home
+// @author      Mike Robinson
+// @homepage    http://twitter.com/akamike
+// ==/UserScript==
+
+(function () {
+  if(window.fluid) {
+    var pageTitle = document.getElementById('page_title');
+    var unread = lastGrowl = 0;
+
+    /*
+     * Dock badge, runs every 1 second
+     */
+    var dockInterval = window.setInterval(function(){
+      var unreadChange = pageTitle.innerText.match(/\d+/);
+
+      if (unreadChange) {
+        if (unreadChange[0] > unread) {
+          unread = unreadChange[0];
+          window.fluid.dockBadge = unread;
+        }
+      } else if (unread != 0) {
+        window.fluid.dockBadge = '';
+        unread = lastGrowl = 0;
+      }
+    }, 1000);
+
+    /*
+     * Growl notification, runs every 10 minutes
+     */
+    var growlInterval = window.setInterval(function(){
+      if (lastGrowl != unread) {
+        lastGrowl = unread;
+
+        window.fluid.showGrowlNotification({
+          title:       'Twitter',
+          description: unread + ' unread tweets',
+          priority:    1,
+          sticky:      false,
+          identifier:  'twitter_unread'
+        });
+      }
+    }, 600000);
+  }
+})();
